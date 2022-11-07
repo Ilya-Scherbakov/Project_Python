1ac18b8cd5f2d70e1d3c48192b47512806d18856
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-11-07 12:53:14 +0400

    seminar 4

--- seminar_3.py
+++ seminar_3.py
@@ -3,7 +3,7 @@
 # def hello(): # чтобы задать функцию пиши def. В скобках писать аргументы кот фун. может принимать. их может и не быть(в данном случае)
 #     print('hello') # эта фун. выполняет принт на экран
 
-# hello() # так вызывается функция 
+# hello() # так вызывается функция
 
 # # -------------------
 
@@ -60,7 +60,7 @@
 # n = int(input('Введи кол-во строк: '))
 # for i in range(n):
 #     a.append(input('теперь вводи их столько раз: '))
-# find_number = input('введите что будешь искать: ')                        
+# find_number = input('введите что будешь искать: ')
 # for i in a:
 #     if find_number in i:
 #         print('да')
@@ -99,15 +99,15 @@
 
 # Задача 22 (ДЗ). Задайте список из нескольких чисел. Программа, кот найдёт сумму эл-ов списка, стоящих на нечётной позиции.
 
-from random import * 
+from random import *
 
 # n = int(input('Введите кол-во элементов: '))
 # some_list = []
-# for _ in range(n):    
-#     some_list.append(randint(-12, 15)) 
+# for _ in range(n):
+#     some_list.append(randint(-12, 15))
 # summ = 0
 # for i in range(1, len(some_list), 2):
-#         summ += some_list[i]       
+#         summ += some_list[i]
 # print(f"{some_list} -> сумма элементов на нечётных позициях: {summ}")
 
 # Решал преподаватель
@@ -123,30 +123,35 @@ from random import *
 #     summa += some_list[idx]
 # print(summa)
 
-# Задача 23 (ДЗ). Программа, которая найдёт произведение пар чисел списка. 
-                # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Задача 23 (ДЗ). Программа, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 # n = int(input('Введите кол-во элементов: '))
 # some_list = []
 # res_list = []
 # summ = 0
-# for _ in range(n):    
-#     some_list.append(randint(-12, 15)) 
+# for _ in range(n):
+#     some_list.append(randint(-12, 15))
 # for i in range(len(some_list) // 2 + (len(some_list) % 2)):
 #     summ = int(some_list[i] * (some_list[len(some_list) - 1 - i]))
 #     res_list.append(summ)
 
 # Решал преподаватель
 
+# count = int(input('введите кол-во элементов: '))
+# some_list = []
+# for _ in range(count):          # тут ввод руками списка
+#     number = int(input())
+#     some_list.append(number)
+# new_list = []
+# for idx in range((count - 1) // 2 + 1):
+#     number1 = some_list[idx]
+#     number2 = some_list[count - idx - 1]
+#     new_list.append(number1 * number2)
+# print(new_list)
 
-
-# print(some_list)
-# print(res_list)
-
-
-
-#  Задача 24 (ДЗ). Задайте список из вещественных чисел. 
-                # Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+#  Задача 24 (ДЗ). Задайте список из вещественных чисел.
+# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
 # from random import uniform
 
@@ -175,6 +180,25 @@ from random import *
 # print(complite)
 # print(dif_min_max(complite))
 
+# Решал преподаватель
+
+# count = int(input('введите кол-во элементов: '))
+# some_list = []
+
+# for _ in range(count):
+#     number = float(input())     # либо тут убрать float и тогда целые числа не бедет брать во внимание
+#     some_list.append(number)
+# maxx = 0
+# minn = 1
+# for el in some_list:
+#     if '.' in str(el):
+#         dr = str(el).split('.')[1]
+#         if float('0.' + dr) > maxx:
+#             maxx = float('0.' + dr)
+#         elif float('0.' + dr) < minn:
+#             minn = float('0.' + dr)
+# print(maxx - minn)
+
 
 # Задача 25 (ДЗ). Программа, которая будет преобразовывать десятичное число в двоичное.
 
@@ -185,28 +209,55 @@ from random import *
 #     n //= 2
 # print(b[::-1])
 
+# Решал преподаватель
+# # вариант 1:
+# n = int(input('введите число: '))
+# print(bin(n)[2:])                       # встроенна функция перевода
+# [2:] это означает начать отображать со втрого символа(3ий) и до конца
+
+# # вариант 2:
+# n = int(input('введите число: '))
+# b = ''
+# while n != 0:
+#     b += str(n % 2) + b                 # тут как-то прибавляя +b получился список развернутым, подумай
+#     n //= 2                             # до этого все считал правильно, просто задом наперед, нужно было развернуть
+# print(b)
+
+
 # Задача 26 (ДЗ). Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
-def Fibonacci(n):
-    if n in [1, 2]:                       
-        return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+# def Fibonacci(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return Fibonacci(n-1) + Fibonacci(n-2)
 
-def NegaFibonacci(n):
-    if n == 1:                       
-        return 1
-    elif n == 2:                       
-        return -1
-    else:
-        num1, num2 = 1, -1
-        for i in range(2, n):
-            num1, num2 = num2, num1 - num2
-        return num2
+# def NegaFibonacci(n):
+#     if n == 1:
+#         return 1
+#     elif n == 2:
+#         return -1
+#     else:
+#         num1, num2 = 1, -1
+#         for i in range(2, n):
+#             num1, num2 = num2, num1 - num2
+#         return num2
 
-list = [0]
-userNumber = int(input('Enter any number: '))
-for e in range(1, userNumber + 1):
-    list.append(Fibonacci(e))
-    list.insert(0, NegaFibonacci(e))
-print(list)
+# list = [0]
+# userNumber = int(input('Enter any number: '))
+# for e in range(1, userNumber + 1):
+#     list.append(Fibonacci(e))
+#     list.insert(0, NegaFibonacci(e))
+# print(list)
+
+# Решал преподаватель
+
+# k = int(input())
+# some_list = [0] * (k * 2 + 1)    # ЗАДАЕМ СПИСОК
+
+# some_list[k + 1] = 1
+# for idx in range(k + 2, k * 2 + 1):
+#     some_list[idx] = some_list[idx - 1] + some_list[idx - 2]
+# for idx in range(k, -1, -1):
+#     some_list[idx] = some_list[idx + 2] - some_list[idx + 1]
+# print(some_list)
